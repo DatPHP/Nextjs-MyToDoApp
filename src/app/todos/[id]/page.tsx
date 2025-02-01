ceb545a178127eabb8022ccb346820ca199f100f
DatPHP <nguyenvandat170296@gmail.com>
2025-02-01 23:29:17 +0700

    Handle id of Detail

--- src/app/todos/[id]/page.tsx
+++ src/app/todos/[id]/page.tsx
@@ -1,12 +1,7 @@
 import React from "react";
 import TodoDetailsContent from "@components/Todo/TodoDetailsContent";
 
-interface PageProps {
-  params: { id: string };
-}
 
-export default async function TodoDetails({ params }: PageProps) {
-  const awaitedParams = await params; // Explicitly await params (if required by Next.js)
-
-  return <TodoDetailsContent id={awaitedParams.id} />;
+export default function TodoDetails({ params }: { params: { id: string } }) {
+  return <TodoDetailsContent id={params?.id} />;
 }
