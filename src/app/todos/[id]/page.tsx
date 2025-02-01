28de59e3bfcfd457dc019d9801236ef3271e354c
DatPHP <nguyenvandat170296@gmail.com>
2025-02-01 23:34:55 +0700

    check Performance again

--- src/app/todos/[id]/page.tsx
+++ src/app/todos/[id]/page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import TodoDetailsContent from "@components/Todo/TodoDetailsContent";
 
+export default async function TodoDetails({ params }: { params: { id: string } }) {
+  // Ensure params is explicitly an object
+  const resolvedParams = typeof params === "object" ? params : await params;
 
-export default function TodoDetails({ params }: { params: { id: string } }) {
-  return <TodoDetailsContent id={params?.id} />;
+  return <TodoDetailsContent id={resolvedParams.id} />;
 }
