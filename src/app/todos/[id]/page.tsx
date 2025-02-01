1d2eca7acc2a0cecffdd42a7b4af68df40cfed45
DatPHP <nguyenvandat170296@gmail.com>
2025-02-01 23:02:18 +0700

    [01/02/2025] Update params for detail page

--- src/app/todos/[id]/page.tsx
+++ src/app/todos/[id]/page.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import TodoDetailsContent from "@components/Todo/TodoDetailsContent";
+import { NextPage } from "next";
 
 interface ITodoDetails {
   params: { id: string };
 }
 
-export default async function TodoDetails({ params }: ITodoDetails) {
-  const { id } = await params; // params is now resolved properly
+const TodoDetails: NextPage<ITodoDetails> = async ({ params }) => {
+  const { id } = params;
 
   return <TodoDetailsContent id={id} />;
-}
+};
+
+export default TodoDetails;
