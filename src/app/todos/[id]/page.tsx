f2b93306eb934677c0b435285016239c3ef5983a
DatPHP <nguyenvandat170296@gmail.com>
2025-02-01 23:40:55 +0700

    DL Again

--- src/app/todos/[id]/page.tsx
+++ src/app/todos/[id]/page.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import TodoDetailsContent from "@components/Todo/TodoDetailsContent";
 
-export default async function TodoDetails({ params }: { params: { id: string } }) {
-  // Ensure params is explicitly an object
-  const resolvedParams = typeof params === "object" ? params : await params;
+interface PageProps {
+  params: { id: string };
+}
 
-  return <TodoDetailsContent id={resolvedParams.id} />;
+export default function TodoDetails({ params }: PageProps) {
+  return <TodoDetailsContent id={params.id} />;
 }
