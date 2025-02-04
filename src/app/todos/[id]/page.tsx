5bb67987c919e1d3231202554ff4c33f83c4fdfe
DatPHP <nguyenvandat170296@gmail.com>
2025-02-04 09:58:36 +0700

    update Dynamic Segment for todos Detail

--- src/app/todos/[id]/page.tsx
+++ src/app/todos/[id]/page.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import TodoDetailsContent from "@components/Todo/TodoDetailsContent";
 
-interface ITodoDetails {
-  params: { id: string };
-}
-
-export default async function TodoDetails({ params }: ITodoDetails) {
-  const { id } = await params; 
-
+export default async function TodoDetails({
+  params,
+}: {
+  params: Promise<{ id: string }>
+}) {
+  const id = (await params).id
   return <TodoDetailsContent id={id} />;
 }
